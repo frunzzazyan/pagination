017e26104a93fca7aeccfad5f37c4759f219cde3
frunzzazyan <frunzzazyan4@gmail.com>
2024-10-27 17:31:21 +0400

    filter users

--- client/src/components/Users.jsx
+++ client/src/components/Users.jsx
@@ -13,36 +13,48 @@ const Users = ({ users, data }) => {
       .then(res => setPage(res))
   }
 
+  function handleSort(){
+    fetch(`http://localhost:3000/sort`)
+    .then(res => res.json())
+    .then(res => setPage(res))
+  }
 
 
   return (
+    <>
+      <button onClick={handleSort}>Sort through all the information </button>
     <div className='users'>
       {(page ? page.map((elem) => {
-        return <div key={elem._id}>
+        return <div className='user' key={elem._id}>
           <h2>
           {elem.first_name}_{elem.last_name}
           </h2>
           <hr />
+          <h3>email_{elem.email}</h3>
+          <h4>age_{elem.age}</h4>
         
         </div>
       }) : users.map((elem) => {
-        return <div key={elem._id}>
+        return <div className='user' key={elem._id}>
           <h2>
           {elem.first_name}_{elem.last_name}
           </h2>
           <hr />
+          <h3>email_{elem.email}</h3>
+          <h4>age_{elem.age}</h4>
         
         </div>
       }))}
 
+        </div>
 
       <div className="pages">
         {pageBtn.fill(null).map((elem,idx)=>{
           return <button onClick={(e) => handleClick(e)} value={idx+1}>{idx+1}</button>
         })}
       </div>
-    </div>
 
+    </>
 
   )
 }
